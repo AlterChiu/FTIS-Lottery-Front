1d8f4a3c56e03b38962fe25dc800c4229a5c3239
台基-繆照慶 <burnettmiao@taiglobal.com.tw>
2023-07-05 10:57:07 +0800

    add btn change text function

--- script/script.js
+++ script/script.js
@@ -4,10 +4,12 @@ const wheel = document.querySelector('.wheel-rotate');
 btn.addEventListener('click', function () {
   wheel.classList.add('active');
   btn.classList.add('scale');
+  btn.innerText = '抽獎中...';
   setTimeout(() => {
     btn.classList.remove('scale');
   }, 100);
   setTimeout(() => {
     wheel.classList.remove('active');
+    btn.innerText = '開始抽獎';
   }, 3000);
 });
