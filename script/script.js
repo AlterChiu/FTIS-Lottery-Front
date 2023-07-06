e813d81b9390ea77774cfb6aea4863e424f13f6f
台基-繆照慶 <burnettmiao@taiglobal.com.tw>
2023-07-06 15:17:55 +0800

    display finish but no random

--- script/script.js
+++ script/script.js
@@ -3,48 +3,46 @@ const wheel = document.querySelector('.wheel-rotate');
 const clearBtn = document.querySelector('.clear');
 const input = document.querySelector('input');
 const display = document.querySelector('.display-content');
-// const icon = document.createElement('i');
-// const span = document.createElement('span');
 
+// 32 person
 const arr = [
-  '資訊室-繆照慶',
-  '資訊室-繆照慶',
-  '資訊室-繆照慶',
-  '資訊室-繆照慶',
-  '資訊室-繆照慶',
-  '資訊室-繆照慶',
-  '資訊室-繆照慶',
-  '資訊室-繆照慶',
-  '資訊室-繆照慶',
-  '資訊室-繆照慶',
-  '資訊室-繆照慶',
-  '資訊室-繆照慶',
-  '資訊室-繆照慶',
-  '資訊室-繆照慶',
-  '資訊室-繆照慶',
-  '資訊室-繆照慶',
-  '資訊室-繆照慶',
-  '資訊室-繆照慶',
-  '資訊室-繆照慶',
-  '資訊室-繆照慶',
-  '資訊室-繆照慶',
-  '資訊室-繆照慶',
-  '資訊室-繆照慶',
-  '資訊室-繆照慶',
-  '資訊室-繆照慶',
-  '資訊室-繆照慶',
-  '資訊室-繆照慶',
-  '資訊室-繆照慶',
-  '資訊室-繆照慶',
-  '資訊室-繆照慶',
-  '資訊室-繆照慶',
-  '資訊室-繆照慶',
+  '資訊室-繆1慶',
+  '資訊室-繆2慶',
+  '資訊室-繆3慶',
+  '資訊室-繆4慶',
+  '資訊室-繆5慶',
+  '資訊室-繆6慶',
+  '資訊室-繆7慶',
+  '資訊室-繆8慶',
+  '資訊室-繆9慶',
+  '資訊室-繆10慶',
+  '資訊室-繆11慶',
+  '資訊室-繆12慶',
+  '資訊室-繆13慶',
+  '資訊室-繆14慶',
+  '資訊室-繆15慶',
+  '資訊室-繆16慶',
+  '資訊室-繆17慶',
+  '資訊室-繆18慶',
+  '資訊室-繆19慶',
+  '資訊室-繆20慶',
+  '資訊室-繆21慶',
+  '資訊室-繆22慶',
+  '資訊室-繆23慶',
+  '資訊室-繆24慶',
+  '資訊室-繆25慶',
+  '資訊室-繆26慶',
+  '資訊室-繆27慶',
+  '資訊室-繆28慶',
+  '資訊室-繆29慶',
+  '資訊室-繆30慶',
+  '資訊室-繆31慶',
+  '資訊室-繆32慶',
 ];
 
 runBtn.addEventListener('click', function () {
-  // clear area
-  // const displayArea = document.querySelector('.display-content');
-  // displayArea.innerHTML = '';
+  const displayArea = document.querySelector('.display-content');
+  displayArea.innerHTML = '';
 
   // button and wheel
   wheel.classList.add('active');
@@ -53,6 +51,7 @@ runBtn.addEventListener('click', function () {
   setTimeout(() => {
     runBtn.classList.remove('scale');
   }, 100);
+
   setTimeout(() => {
     wheel.classList.remove('active');
 
@@ -95,7 +94,7 @@ runBtn.addEventListener('click', function () {
   console.log(input.value);
 });
 
-// clear
+// clear btn
 clearBtn.addEventListener('click', function () {
   const displayArea = document.querySelector('.display-content');
   displayArea.innerHTML = '';
